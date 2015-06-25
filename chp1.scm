2ded1eb68eeb69783b8e8c719971bfb1680e7e35
cappachu <cappachu@gmail.com>
2015-06-25 17:23:13 -0400

    Ex 1.32 high order accumulate procedure

--- chp1.scm
+++ chp1.scm
@@ -484,7 +484,21 @@
   (* 4.0 (product-iter pi-term 1 inc 1000)))
 
 
+; Exercise 1.32
 
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner null-value term (next a) next b))))
+
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner (term a) result))))
+  (iter a null-value))
+  
   
 
 

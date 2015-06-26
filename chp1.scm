5fd7290e99cbbadb07ca0cbc8ec757c588a29d24
cappachu <cappachu@gmail.com>
2015-06-26 16:09:33 -0400

    Ex 1.35 golden ratio using fixed-point

--- chp1.scm
+++ chp1.scm
@@ -505,7 +505,28 @@
 (define (product-accumulate term a next b)
   (accumulate-iter * 1 term a next b))
 
+; Exercise 1.35 pg 70
+; 
+; 1 + 1/phi
+; = (phi + 1) / phi
+; = phi^2 / phi [since phi^2 = phi + 1 (page 38)]
+; = phi
+;
+(define tolerance 0.00001)
 
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+  
+(define (golden-ratio)
+  (fixed-point (lambda (x) (+ 1 (/ 1 x)))
+               1.0))
 
 
 

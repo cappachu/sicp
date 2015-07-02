9999ce9f64cd4aa382319671f060d1f50637c9f2
cappachu <cappachu@gmail.com>
2015-07-02 14:53:01 -0400

    2.1.2 Abstraction Barriers, Ex 2.2, 2.3

--- chp2.scm
+++ chp2.scm
@@ -58,6 +58,97 @@
       (let ((sign (if (< d 0) - +)))
         (cons (sign numer) (sign denom))))))
 
+;; Exercise 2.2 pg 89
+
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (midpoint-segment s)
+  (define (average a b)
+    (/ (+ a b) 2.0))
+  (let ((x-start (x-point (start-segment s)))
+        (y-start (y-point (start-segment s)))
+        (x-end (x-point (end-segment s)))
+        (y-end (y-point (end-segment s))))
+    (make-point (average x-start x-end) (average y-start y-end))))
+
+;; (midpoint-segment (make-segment (make-point 10 10)
+;;                                 (make-point 0 0)))
+
+;; Exercise 2.3 pg 90
+(define (make-rect left-segment bottom-segment)
+  (cons left-segment bottom-segment))
+
+(define (left-segment r)
+  (car r))
+
+(define (bottom-segment r)
+  (cdr r))
+
+(define (length s)
+  (define (square x) (* x x))
+  (let ((x-start (x-point (start-segment s)))
+        (y-start (y-point (start-segment s)))
+        (x-end (x-point (end-segment s)))
+        (y-end (y-point (end-segment s))))
+    (sqrt (+ (square (- y-end y-start))
+             (square (- x-end x-start))))))
+
+(define (area r)
+  (* (length (left-segment r))
+     (length (bottom-segment r))))
+
+(define (perimeter r)
+  (+ (* 2 (length (left-segment r)))
+     (* 2 (length (bottom-segment r)))))
+
+;; We could just as easily have used right-segment and top-segment
+;; to represent a rectangle and we would have to change just
+;; the area and perimeter procedures. However, if we represented rectangles with
+;; two points for example, we would need to change the area and perimeter procedures.
+
+;; (area (make-rect (make-segment (make-point 0 0)
+;;                                (make-point 0 5))
+;;                  (make-segment (make-point 0 0)
+;;                                (make-point 10 0))))
+
+;; (perimeter (make-rect (make-segment (make-point 0 0)
+;;                                     (make-point 0 5))
+;;                       (make-segment (make-point 0 0)
+;;                                     (make-point 10 0))))
+
+
+
+
+
+
+
+
+
+
 
 
         
